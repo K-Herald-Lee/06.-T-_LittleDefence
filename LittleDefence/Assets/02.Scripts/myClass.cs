064fe0a767f1e6b533cc20e5c7a2999bf41c6055
K-Herald-Lee <herald.lee1118@gmail.com>
2022-12-01 23:44:26 -0500

    update
    Add BGM and SFX
    Add sound control manager

--- LittleDefence/Assets/02.Scripts/myClass.cs
+++ LittleDefence/Assets/02.Scripts/myClass.cs
@@ -1,4 +1,5 @@
 public class myClass 
 {
-    public enum Unit {Swordman, Archer, Guard, Wizard, Bullet};
+    public enum Unit {Swordman, Archer, Guard, Wizard, Bullet, Base};
+    public enum Sound {Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic};
 }
